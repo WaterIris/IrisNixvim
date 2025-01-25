793f63cf05be1e4840159f35f0feba965145858c
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-01-26 00:25:31 +0100

    add basic settings and colorscheme

---
+++ config/options.nix
@@ -0,0 +1,16 @@
+{
+  config = {
+    opts = {
+      updatetime = 100;
+      number = true;
+      relativenumber= true;
+      clipboard = "unnamedplus";
+      cursorline = true;
+      expandtab = true;
+      autoindent = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      undolevels = 1000;
+    };
+  };
+}
