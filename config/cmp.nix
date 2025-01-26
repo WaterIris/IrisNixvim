7abeab035fcd81ada3b6ca7d8c92f094ea7afc3a
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-01-27 00:40:38 +0100

    cmp working

--- config/cmp.nix
+++ config/cmp.nix
@@ -2,27 +2,37 @@
   plugins.luasnip = {
     enable = true;
   };
-
   plugins.cmp = {
-    enable = true;
-    autoEnableSources = true;
-    settings = {
-      mapping = {
-          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<C-e>" = "cmp.mapping.abort()";
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
+      enable = true;
+      autoEnableSources = true;
+      settings = {
+        snippet = {
+          expand = /* lua */ ''
+            function(args)
+            require("luasnip").lsp_expand(args.body)
+            end
+            '';
+        };
+
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "path"; }
+          { name = "buffer"; }
+          { name = "luasnip"; }
+        ];
+
+        mapping = {
+          __raw = ''
+            cmp.mapping.preset.insert({
+              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<C-e>'] = cmp.mapping.abort(),
+              ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            })
+          '';
+        };
       };
-      snippet = {
-        expand = "function(args) require('luasnip').lsp_expand(args.body) end";
-      };
-      sources = [
-        {name = "nvim_lsp";}
-        {name = "path";}
-        {name = "luasnip";}
-        {name = "buffer";}
-      ];
-    };
-  };
+  }; 
+ 
 }
