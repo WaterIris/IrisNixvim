fcd1eb4573dc85d651cce30b8b9de94e108d27f7
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-01-27 00:16:57 +0100

    tweaked indent-blankline

--- config/indent_blankline.nix
+++ config/indent_blankline.nix
@@ -1,5 +1,13 @@
 {
   plugins.indent-blankline = {
     enable = true;
+    settings = {
+      scope = {
+        enabled = false;
+        show_end = false;
+        show_exact_scope = false;
+        show_start = false;
+      };
+    };
   };
 }
