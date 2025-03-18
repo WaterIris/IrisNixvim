9bce3235d2b28041f8830d7a5e1743e85aec767b
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-03-18 21:46:10 +0100

    add color modification

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -6,16 +6,16 @@
       base01 = "#171b24";  # current line 
       base02 = "#171b24";
       base03 = "#3A435A";  # comments
-      base04 = "#956dca";  #changed 
-      base05 = "#956dca";
-      base06 = "#f4f3ee";
-      base07 = "#f4f3ee";
-      base08 = "#f4f3ee";
+      base04 = "#956dca";  # changed 
+      base05 = "#956dca";  # brackes and finction parameter definitions
+      base06 = "#f4f3ee";  # no clue
+      base07 = "#f4f3ee";  # no clue
+      base08 = "#f4f3ee";  # variable names
       base09 = "#5599E2";  # Capital lettern and ints
       base0A = "#956dca";  # range itp
-      base0B = "#5679E3";
+      base0B = "#40a02b";  # strings
       base0C = "#5679E3";
-      base0D = "#DE642B";
+      base0D = "#ed8796";  # function names, also for buit int
       base0E = "#3F8CDE";
       base0F = "#f4f3ee";
     };
