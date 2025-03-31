15148114a54d6cfc029c64f0dee8e9a61c0d8db8
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-03-31 12:49:22 +0200

    variable color change

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -6,18 +6,18 @@
       base01 = "#171b24";  # current line 
       base02 = "#171b24";
       base03 = "#3A435A";  # comments
-      base04 = "#f4f3ee";  # changed 
-      base05 = "#f4f3ee";  # brackes and finction parameter definitions
-      base06 = "#f4f3ee";  # no clue
-      base07 = "#f4f3ee";  # no clue
-      base08 = "#f4f3ee";  # variable names
+      base04 = "#d7d9d7";  # changed 
+      base05 = "#d7d9d7";  # brackes and finction parameter definitions
+      base06 = "#d7d9d7";  # no clue
+      base07 = "#d7d9d7";  # no clue
+      base08 = "#d7d9d7";  # variable names
       base09 = "#5599E2";  # Capital lettern and ints
       base0A = "#e5c890";  # Classes range itp
       base0B = "#83C092";  # strings
       base0C = "#5679E3";
       base0D = "#E67E80";  # function names, also for buit int
       base0E = "#3F8CDE";
-      base0F = "#f4f3ee";
+      base0F = "#d7d9d7";
     };
   };
 }
