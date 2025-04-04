a69e29d9e05d8d82055ac268fe98615cc19b4b33
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-04-04 18:25:05 +0200

    back

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -1,5 +1,23 @@
 {
-  colorschemes.gruvbox = {
+  colorschemes.base16 = {
     enable = true;
+    colorscheme = {
+      base00 = "#101319";
+      base01 = "#171b24";  # current line 
+      base02 = "#171b24";
+      base03 = "#3A435A";  # comments
+      base04 = "#d7d9d7";  # changed 
+      base05 = "#d7d9d7";  # brackes and finction parameter definitions
+      base06 = "#d7d9d7";  # no clue
+      base07 = "#d7d9d7";  # no clue
+      base08 = "#d7d9d7";  # variable names
+      base09 = "#5599E2";  # Capital lettern and ints
+      base0A = "#e5c890";  # Classes range itp
+      base0B = "#83C092";  # strings
+      base0C = "#5679E3";
+      base0D = "#E67E80";  # function names, also for buit int
+      base0E = "#3F8CDE";
+      base0F = "#d7d9d7";
+    };
   };
 }
