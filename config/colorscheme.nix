cfd2c1fc45146f650c7b870dd77884bbfa8fc751
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-04-05 01:47:24 +0200

    recolor

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -3,21 +3,21 @@
     enable = true;
     colorscheme = {
       base00 = "#101319";
-      base01 = "#171b24";  # current line 
-      base02 = "#171b24";
-      base03 = "#3A435A";  # comments
-      base04 = "#d7d9d7";  # changed 
-      base05 = "#956dca";  # brackes and finction parameter definitions
-      base06 = "#d7d9d7";  # no clue
-      base07 = "#d7d9d7";  # no clue
-      base08 = "#d7d9d7";  # variable names
-      base09 = "#5599E2";  # Capital lettern and ints
+      base01 = "#171b24";  # Line 
+      base02 = "#171b24";  # Check !!
+      base03 = "#5b6078";  # Comments
+      base04 = "#e7ebf6";  # changed 
+      base05 = "#b77ed1";  # brackes and finction parameter definitions
+      base06 = "#fb8000";  # no clue
+      base07 = "#fb8000";  # no clue
+      base08 = "#e7ebf6";  # Variable names
+      base09 = "#56b8f7";  # Capital lettern and ints
       base0A = "#e5c890";  # Classes range itp
       base0B = "#83C092";  # strings
-      base0C = "#5679E3";
-      base0D = "#E67E80";  # function names, also for buit int
-      base0E = "#3F8CDE";
-      base0F = "#d7d9d7";
+      base0C = "#fb8000";
+      base0D = "#fb80aa";  # function names, also for buit int
+      base0E = "#89cdfc";  # floats ints etc it seems
+      base0F = "#bcc6e3";
     };
   };
 }
