1f451945d44030fd68dbfade601cd62aa21d8c34
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-07-12 11:11:33 +0200

    Add back some colors

--- config/theme.nix
+++ config/theme.nix
@@ -8,16 +8,16 @@
       base02 = "#44465c"; # Selection Background
       base03 = "#9699b7"; # Comments, Invisibles, Line Highlighting
       base04 = "#d9e0ee"; # Dark Foreground (Used for status bars)
-      base05 = "#d9e0ee"; # Default Foreground, Caret, Delimiters, Operators
+      base05 = "#d9e0ee"; # Variable names
       base06 = "#d9e0ee"; # Light Foreground (Not often used)
       base07 = "#d9e0ee"; # Light Background (Not often used)
       base08 = "#d9e0ee"; # Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
       base09 = "#d9e0ee"; # Integers, Boolean, Constants, XML Attributes, Markup Link Url
-      base0A = "#d9e0ee"; # Classes, Markup Bold, Search Text Background
-      base0B = "#d9e0ee"; # Strings, Inherited Class, Markup Code, Diff Inserted
-      base0C = "#d9e0ee"; # Support, Regular Expressions, Escape Characters, Markup Quotes
-      base0D = "#d9e0ee"; # Functions, Methods, Attribute IDs, Headings
-      base0E = "#d9e0ee"; # Keywords, Storage, Selector, Markup Italic, Diff Changed
+      base0A = "#fb80aa"; # Classes,
+      base0B = "#83C092"; # Strings
+      base0C = "#e5c890"; # Support, Regular Expressions, Escape Characters, Markup Quotes
+      base0D = "#56b8f7"; # Functions
+      base0E = "#52a9a9"; # Keywords, Storage, Selector, Markup Italic, Diff Changed
       base0F = "#d9e0ee"; # Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
     };
   };
