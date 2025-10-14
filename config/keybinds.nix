7a925a2099ef356a41cb04d60cccee537809745b
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-10-15 01:20:14 +0200

    even more update

--- config/keybinds.nix
+++ config/keybinds.nix
@@ -9,5 +9,17 @@
 	desc = "Enter file explorer";
       };
     }
+    {
+      mode = "x";
+      key = "<leader>p";
+      action = "\"_dP";
+      options.desc = "Paste without changing registers content";
+    }
+    {
+      mode = "x";
+      key = "<leader>d";
+      action = "\"_d";
+      options.desc = "Delete without changing registers content";
+    }
   ];
 }
