07d16745d097064e7ad429dff025d2f20f174246
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-10-14 22:31:12 +0200

    new hotkeys

--- config/lsp.nix
+++ config/lsp.nix
@@ -3,6 +3,43 @@
     lsp = {
       enable = true;
       inlayHints = true;
+      keymaps = {
+        silent = true;
+        diagnostic = {
+          "<leader>j" = "goto_next";
+          "<leader>k" = "goto_prev";
+        };
+        lspBuf = {
+          "rn" = "rename"; 
+        };
+        extra = [
+          {
+            action = {
+            __raw = "require('telescope.builtin').lsp_definitions";
+            };
+            key = "gd";
+          }
+          {
+            action = {
+            __raw = "require('telescope.builtin').lsp_references";
+            };
+            key = "gr";
+          }
+          {
+            action = {
+            __raw = "require('telescope.builtin').lsp_implementations";
+            };
+            key = "gi";
+          }
+          {
+            action = {
+            __raw = "require('telescope.builtin').lsp_type_definitions";
+            };
+            key = "gt";
+          }
+
+        ];
+      };
       servers = {
 	nixd = {
 	  enable = true;
